e17f220a3454001c3b189f3ffe9d95b41ccf1215
kbr_1218 <kbr_uni@naver.com>
2025-02-13 12:34:47 +0900

    [FIX] 포스터 경로 수정

--- src/components/movie_bg/movie_background.jsx
+++ src/components/movie_bg/movie_background.jsx
@@ -3,14 +3,22 @@ import React from "react";
 import styles from "./movieBackground.module.css";
 
 const MovieBackground = ({ selectedMovie }) => {
+  if (!selectedMovie) return null;
+
+  // 배경 이미지 URL 설정 (backdrop_path > poster_path 우선순위)
+  let backgroundUrl = "none";
+
+  if (selectedMovie.backdrop_path) {
+    backgroundUrl = selectedMovie.backdrop_path.startsWith("http")
+      ? selectedMovie.backdrop_path
+      : `https://image.tmdb.org/t/p/original${selectedMovie.backdrop_path}`;
+  }
+
   return (
     <div 
       className={styles.background}
       style={{
-        backgroundImage: selectedMovie?.backdrop_path
-        ? `url(https://image.tmdb.org/t/p/original${selectedMovie.backdrop_path})`
-        : "none"
-      }}
+        backgroundImage: backgroundUrl !== "none" ? `url(${backgroundUrl})` : "none" }}
     >
       {/* 전체적으로 어두운 배경을 적용할 오버레이 */}
       <div className={styles.darkOverlay}></div>
--- src/components/poster/moimg.jsx
+++ src/components/poster/moimg.jsx
@@ -7,14 +7,23 @@ const Moimg = ({ selectedMovie }) => {
     return null;
   }
 
+  // 포스터 이미지 URL 설정
+  const posterUrl = selectedMovie.poster_path?.startsWith("http")
+    ? selectedMovie.poster_path
+    : `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`;
+
   return (
     <div class={styles.poster_container}>
-      {selectedMovie.poster_path && (
+      {selectedMovie.poster_path? (
         <img
-          src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`}
+          src={posterUrl}
           alt={selectedMovie.title}
           className={styles.poster_img}
         />
+      ) : (
+        <div className={styles.poster_placeholder}>
+          {selectedMovie.title}
+        </div>
       )}
       <button className={styles.watch_btn}>시청하기</button>
     </div>
